bfd050842467ac19b751b656f3792320a9426407
Antony Shaleynikov <shaleynikov@gmail.com>
2015-03-19 14:13:19 +0300

    feat(tests) changed expectjs library to chai

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,11 +1,19 @@
-var expect = require("expect.js");
+var expect = require("chai").expect;
+var _ = require('lodash');
 
 var algo = require('../cloud/algorithm/interface.js')
 
-describe('Message', function () {
-  describe('response', function () {
-    it('should have a function defined', function () {
-      expect(algo.get_results).to.be.a('function');
+describe('Cloud points', function () {
+  describe('Weight loss algorithm proxy', function () {
+    it('should have methods defined', function () {
+      var methodsCount = 0;
+
+      _.forEach(['getChallenges', 'getAlgoData', 'fetchDataFromAlgorithm', 'isProfileComplete'], function (method) {
+        expect(algo[method]).to.be.a('function');
+        methodsCount++;
+      });
+
+      expect(methodsCount).to.equal(4);
     });
   });
 });
