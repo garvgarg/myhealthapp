d5a4282f4be3d9a7617a616f62bb08b72ef828ec
Antony Shaleynikov <shaleynikov@gmail.com>
2015-03-24 02:02:29 +0300

    feat(CompletionRate): now challenges completed over period are grouped by focus id

--- cloud/challenges/completion.js
+++ cloud/challenges/completion.js
@@ -3,6 +3,21 @@ var _ = require('underscore');
 var moment = require('moment');
 
 /**
+ * Safe division preventing division to zero
+ *
+ * @param a
+ * @param b
+ * @returns {number}
+ * @private
+ */
+function _safe_perc(a, b) {
+  if (b !== 0) {
+    return Math.round(a / b * 100);
+  }
+  return 0;
+}
+
+/**
  * Calculate Challenges Completion Rate over 2 week periods
  *
  * @param req
@@ -14,54 +29,73 @@ function calculateChallengesCompletionRateOverPeriod(req, res) {
 
   // Instantiate UserChallenges object
   var UserChallenges = Parse.Object.extend('UserChallenges');
-  var query = new Parse.Query(UserChallenges);
 
-  query.equalTo('user', currentUser);
+  // Fetch all challenges
+  var Challenges = Parse.Object.extend("Challenges");
+  var challengesQuery = new Parse.Query(Challenges);
+  challengesQuery.include(['GroupID', 'GroupID.FocusID']);
+  challengesQuery.find().then(function (challenges) {
+    var query = new Parse.Query(UserChallenges);
 
-  // Fetch challenges associated with current user
-  // and filter them one-by-one
-  query.find().then(function (data) {
-    var results = {'completed_2weeks': [], 'completed_4weeks': [], 'accepted_2weeks': [], 'accepted_4weeks': []};
-    _.each(data, function (e) {
+    query.equalTo('user', currentUser);
 
-      // Calculate amount of day passed since the challenge was completed
-      var days = moment().diff(e.get('completedAt'), 'days');
-      if (e.get('completed')) {
-        if (days < 14) {
-          results['completed_2weeks'].unshift(e);
+    // Fetch challenges associated with current user
+    // and filter them one-by-one
+    query.find().then(function (data) {
+      // ASSUMPTION: Focus id is predefined entitites
+      var results = {
+        'completed_2weeks': {1: [], 2: [], 3: []},
+        'completed_4weeks': {1: [], 2: [], 3: []},
+        'accepted_2weeks': {1: [], 2: [], 3: []},
+        'accepted_4weeks': {1: [], 2: [], 3: []}
+      };
+      _.each(data, function (e) {
+
+        // Get challenge from challenges list
+        var currentChallenge = _.find(challenges, function (c) {
+          return c.get('challengeId') == e.get('challengeId');
+        });
+
+        if (currentChallenge) {
+
+          var currentFocus = currentChallenge.get('GroupID').get('FocusID').get("focusId");
+          // Calculate amount of day passed since the challenge was completed
+          var days = moment().diff(e.get('completedAt'), 'days');
+          if (e.get('completed')) {
+            if (days < 14) {
+              results['completed_2weeks'][currentFocus].unshift(e);
+            }
+            if (days < 28 && days >= 14) {
+              results['completed_4weeks'][currentFocus].unshift(e);
+            }
+          }
+
+          // Calculate amount of day passed since the challenge was accepted
+          var days2 = moment().diff(e.get('acceptedAt'), 'days');
+
+          if (days2 < 14) {
+            results['accepted_2weeks'][currentFocus].unshift(e);
+          }
+          if (days2 < 28 && days2 >= 14) {
+            results['accepted_4weeks'][currentFocus].unshift(e);
+          }
         }
-        if (days < 28 && days >= 14) {
-          results['completed_4weeks'].unshift(e);
-        }
-      }
+      });
 
-      // Calculate amount of day passed since the challenge was accepted
-      var days2 = moment().diff(e.get('acceptedAt'), 'days');
+      results['completionRate_2weeks'] = {};
+      results['completionRate_4weeks'] = {};
 
-      if (days2 < 14) {
-        results['accepted_2weeks'].unshift(e);
-      }
-      if (days2 < 28 && days2 >= 14) {
-        results['accepted_4weeks'].unshift(e);
-      }
+      // For each focus id
+      // ASSUMPTION: Focus id is predefined entitites
+      _.each([1, 2, 3], function (focusId) {
+        results['completionRate_2weeks'][focusId] = _safe_perc(results['completed_2weeks'][focusId].length, results['accepted_2weeks'][focusId].length);
+        results['completionRate_4weeks'][focusId] = _safe_perc(results['completed_4weeks'][focusId].length, results['accepted_4weeks'][focusId].length);
+      });
+
+      res.success(results);
+
     });
-
-    // Prevent division to zero
-    if (results['accepted_2weeks'].length) {
-      results['completionRate_2weeks'] = Math.round(results['completed_2weeks'].length / results['accepted_2weeks'].length * 100);
-    } else {
-      results['completionRate_2weeks'] = 0;
-    }
-
-    // Prevent division to zero
-    if (results['accepted_4weeks'].length) {
-      results['completionRate_4weeks'] = Math.round(results['completed_4weeks'].length / results['accepted_4weeks'].length * 100);
-    } else {
-      results['completionRate_4weeks'] = 0;
-    }
-
-    res.success(results);
-  })
+  });
 }
 
 
